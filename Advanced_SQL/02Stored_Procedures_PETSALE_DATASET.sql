e65f7f08b9d1c79c9c1154d781cff755198adb37
KemanXiang <931132891@qq.com>
2026-07-23 17:43:51 +0800

    Update and rename Stored_Procedures_PETSALE_DATASET.sql to 02Stored_Procedures_PETSALE_DATASET.sql

--- Advanced_SQL/02Stored_Procedures_PETSALE_DATASET.sql
+++ Advanced_SQL/02Stored_Procedures_PETSALE_DATASET.sql
@@ -2,6 +2,9 @@
 #1.Create stored procedures
 #2.Execute stored procedures
 
+#Introduction: Stored Procedures
+#Stored Procedures in SQL are a type of database object that allow you to encapsulate a series of SQL statements into a single routine. They are stored in the database data dictionary and can be invoked from an application program or from the database command interface. Stored procedures can accept input parameters and return multiple values of output parameters. They can also include control-of-flow constructs such as loops and conditional statements.
+#Stored procedures offer several benefits including improved performance, higher productivity, ease of use, and increased scalability. They also provide a mechanism for enforcing business rules and data integrity in the database system.
 
 DROP TABLE IF EXISTS PETSALE;
 
